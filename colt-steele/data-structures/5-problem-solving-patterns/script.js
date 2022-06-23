84236a9d9fc6a66f3c0481e39bf40d23e60aa050
dfung003 <dfung003@ucr.edu>
2022-06-23 12:10:05 -0700

    multiple pointers, sliding window, divide and conquer notes

--- colt-steele/data-structures/5-problem-solving-patterns/script.js
+++ colt-steele/data-structures/5-problem-solving-patterns/script.js
@@ -109,3 +109,156 @@ function validAnagram(first, second) {
 // {a: 3, n: 1, g: 1, r: 1, m: 1}
 // {a: 0, n: 0, g: 0, r: 0, m: 0}
 validAnagram('anagram', 'nagaram')
+
+// Multiple Pointers
+
+// Creating pointers or values that correspond to an index or position move towards the beginning, end or middle based on a certain condition
+
+// Very efficient for solving problems with minimal space complexity as well
+
+// Example 2
+
+/*
+
+Write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist
+
+sumZero([-3 -2, -1, 0, 1, 2, 3]) // [-3, 3]
+sumZero([-2, 0, 1, 3]) // undefined
+sumZero([1, 2, 3]) // undefined
+
+*/
+
+function sumZero(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+        if (sum === 0) {
+            return [arr[left], arr[right]];
+        } else if (sum > 0) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+}
+
+sumZero([-4, -3, -2, -1, 0, 1, 2, 3, 10])
+
+// Time Complexity O(n), Space Complexity O(1)
+
+// countUniqueValues
+
+/*
+
+Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted.
+
+countUniqueValues([1, 1, 1, 1, 1, 1, 2]) // 2
+countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]) // 7
+countUniqueValues([]) // 0
+countUniqueValues([-2, -1, -1, 0, 1]) // 4
+
+*/
+
+function countUniqueValues(arr) {
+    if (arr.length === 0) return 0;
+    let i = 0;
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j]
+        }
+    }
+    return i + 1
+}
+
+countUniqueValues([1, 1, 2, 3, 3, 4, 5, 6, 6, 7])
+
+// Time complexity O(n)
+
+// Sliding Window
+
+/*
+
+- This pattern involves creating a window which can either be an array or number from one position to another
+- Depending on a certain condition, the window either increases or closes (and a new window is created)
+- Very useful for keeping track of a suset of data in an array/string etc.
+
+*/
+
+// Example 3
+
+// Write a function called maxSubarraySum which accepts an array of integers and a number called n. The function should calculate the maximum sum of n consecutive elements in the array.
+
+/*
+
+maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2) // 10
+maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4) // 17
+maxSubarraySum([4, 2, 1, 6], 1) // 6
+maxSubarraySum([4, 2, 1, 6, 2], 4) // 13
+maxSubarraySum([], 4) // null
+
+*/
+
+function maxSubarraySum(arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < num) return null;
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+}
+
+// Time Complexity O(n)
+
+// Divide and Conquer Pattern
+
+/*
+
+- This pattern involves dividing a data set into smaller chunks and then repeating a process with a subset of data
+- This pattern can tremendously decrease time complexity
+
+*/
+
+// Example 4
+
+// Given a sorted array of integers, write a function called search, that accepts a value and returns the index where the value passed to the function is located. If the value is not found, return -1
+
+/*
+
+search([1, 2, 3, 4, 5, 6], 4) // 3
+search([1, 2, 3, 4, 5, 6], 6) // 5
+search([1, 2, 3, 4, 5, 6], 11) // -1
+
+*/
+
+// Refactored solution using Binary Search
+
+function search(array, val) {
+    let min = 0;
+    let max = array.length - 1;
+
+    while (min <= max) {
+        let middle = Math.floor((min + max) / 2);
+        let currentElement = array[middle];
+
+        if (array[middle] < val) {
+            min = middle + 1;
+        }
+        else if (array[middle] > val) {
+            max = middle - 1;
+        }
+        else {
+            return middle;
+        }
+    }
+    return -1;
+}
+
+// Time Complexity Log N
