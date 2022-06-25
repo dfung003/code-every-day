470b12353e40dd076195a007eb6f7bf03fb4c836
dfung003 <dfung003@ucr.edu>
2022-06-24 18:36:10 -0700

    solved chunks in algoscast

---
+++ arthurs-algos/chunk/script.js
@@ -0,0 +1,20 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, size) {
+    const arrChunk = [];
+    for (let i = 0; i < array.length; i += size) {
+        const chunkSize = array.slice(i, i + size);
+        arrChunk.push(chunkSize);
+    }
+    return arrChunk;
+}
+
+console.log(chunk([1, 2, 3, 4, 5], 2))
