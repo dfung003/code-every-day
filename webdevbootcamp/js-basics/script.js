473dbc9b97cdd2cbae803b5cb1b488eb4fda4bad
dfung003 <dfung003@ucr.edu>
2022-07-09 14:11:56 -0700

    web dev bootcamp js-basics

---
+++ webdevbootcamp/js-basics/script.js
@@ -0,0 +1,51 @@
+// Primitive Types
+
+/*
+
+- Number
+- String
+- Boolean
+- Null
+- Undefined
+- Symbol
+- BigInt
+
+*/
+
+// Numbers
+
+/*
+- JS has one number type
+- Positive numbers
+- Negative numbers
+- Whole numbers (integers)
+- Decimal numbers
+*/
+
+// NaN
+
+// NaN is a numeric value that represents somethign that is not a number
+
+// Variables
+
+/*
+- We can store a value and give it a name so we can:
+- refer back to it later
+- use that value to do... stuff
+- or change it later 
+*/
+
+// Basic Syntax
+
+let someName = value;
+
+let year = 1985;
+// make a variable called "year" and give it the value of 1985
+// if you type year in console, it will output the value 1985
+
+let numHens = 5
+let numRoosters = 3
+
+totalChickens = numHens + numRoosters
+
+console.log(totalChickens)
