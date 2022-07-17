d1c409bf42370fe9c94b455c2e07e3fab4d82ed3
dfung003 <dfung003@ucr.edu>
2022-07-17 12:15:45 -0700

    factorial recursion problem

--- colt-steele/data-structures/8-recursion-problems/script.js
+++ colt-steele/data-structures/8-recursion-problems/script.js
@@ -7,3 +7,14 @@ function power(base, exponent) {
     if (exponent === 0) return 1;
     return base * power(base, exponent - 1);
 }
+
+//factorial(1) // 1
+// factorial(2) // 2
+// factorial(4) // 24
+// factorial(7) // 5040
+
+function factorial(x) {
+    if (x < 0) return 0;
+    if (x <= 1) return 1;
+    return x * factorial(x - 1);
+}
