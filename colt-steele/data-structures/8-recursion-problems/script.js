305933b4cdc0d0ed233ac89580868571f783c669
dfung003 <dfung003@ucr.edu>
2022-07-19 10:30:48 -0700

    product of array solution

--- colt-steele/data-structures/8-recursion-problems/script.js
+++ colt-steele/data-structures/8-recursion-problems/script.js
@@ -18,3 +18,14 @@ function factorial(x) {
     if (x <= 1) return 1;
     return x * factorial(x - 1);
 }
+
+
+// productOfArray([1,2,3]) // 6
+// productOfArray([1,2,3,10]) // 60
+
+function productOfArray(arr) {
+    if (arr.length === 0) {
+        return 1;
+    }
+    return arr[0] * productOfArray(arr.slice(1));
+}
