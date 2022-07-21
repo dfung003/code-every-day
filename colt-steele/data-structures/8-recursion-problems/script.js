2001630082cfb4c775b4f98f49c24263d189abe9
dfung003 <dfung003@ucr.edu>
2022-07-21 12:31:23 -0700

    recursive range solution

--- colt-steele/data-structures/8-recursion-problems/script.js
+++ colt-steele/data-structures/8-recursion-problems/script.js
@@ -29,3 +29,12 @@ function productOfArray(arr) {
     }
     return arr[0] * productOfArray(arr.slice(1));
 }
+
+// SAMPLE INPUT/OUTPUT
+// recursiveRange(6) // 21
+// recursiveRange(10) // 55 
+
+function recursiveRange(x) {
+    if (x === 0) return 0;
+    return x + recursiveRange(x - 1);
+}
