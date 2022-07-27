d5110f84b1d56fe03b4623d9ac107e99fe86ea4b
dfung003 <dfung003@ucr.edu>
2022-07-27 13:31:47 -0700

    recursiveRange and fib solution fix

--- colt-steele/data-structures/8-recursion-problems/script.js
+++ colt-steele/data-structures/8-recursion-problems/script.js
@@ -9,7 +9,7 @@ function power(base, exponent) {
 }
 
 // Write a function factorial which accepts a number and returns the factorial of that number. A factorial is the product of an integer and all the integers below it
-//factorial(1) // 1
+// factorial(1) // 1
 // factorial(2) // 2
 // factorial(4) // 24
 // factorial(7) // 5040
@@ -31,6 +31,7 @@ function productOfArray(arr) {
     return arr[0] * productOfArray(arr.slice(1));
 }
 
+// Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function
 // SAMPLE INPUT/OUTPUT
 // recursiveRange(6) // 21
 // recursiveRange(10) // 55 
@@ -40,6 +41,7 @@ function recursiveRange(x) {
     return x + recursiveRange(x - 1);
 }
 
+// Write a recursive function called fib which accepts a number and returns the nth number in the Fibonacci sequence. Recall that the fibonacci sequence is the sequence of the whole numbers 1, 1, 2, 3, 5, 8, ... which starts with 1 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
 // fib(4) // 3
 // fib(10) // 55
 // fib(28) // 317811
