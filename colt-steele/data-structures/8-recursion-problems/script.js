c921298c15da5b58c55e112d2689bdb49df1d611
dfung003 <dfung003@ucr.edu>
2022-08-02 10:59:24 -0700

    palindrome solution

--- colt-steele/data-structures/8-recursion-problems/script.js
+++ colt-steele/data-structures/8-recursion-problems/script.js
@@ -56,5 +56,20 @@ function reverse(str) {
     if (str.length <= 1) return str;
     return reverse(str.slice(1)) + str[0];
 }
-  // reverse('awesome') // 'emosewa'
-  // reverse('rithmschool') // 'loohcsmhtir'
+// reverse('awesome') // 'emosewa'
+// reverse('rithmschool') // 'loohcsmhtir'
+
+// isPalindrome('awesome') // false
+// isPalindrome('foobar') // false
+// isPalindrome('tacocat') // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false
+
+// Write a recursive function called isPalindrome which returns true if the string passed to it is a palindrome (reads the same forward and backward). Otherwise it returns false.
+
+function isPalindrome(str) {
+    if (str.length === 1) return true;
+    if (str.length === 2) return str[0] === str[1];
+    if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1))
+    return false;
+}
