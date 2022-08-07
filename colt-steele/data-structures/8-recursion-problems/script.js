65d3807d0541cd85f163c554e09767c6f075e660
dfung003 <dfung003@ucr.edu>
2022-08-06 17:56:43 -0700

    flatten solution

--- colt-steele/data-structures/8-recursion-problems/script.js
+++ colt-steele/data-structures/8-recursion-problems/script.js
@@ -87,3 +87,21 @@ function someRecursive(array, callback) {
     if (callback(array[0])) return true;
     return someRecursive(array.slice(1), callback);
 }
+
+function flatten(oldArr) {
+    var newArr = []
+    for (var i = 0; i < oldArr.length; i++) {
+        if (Array.isArray(oldArr[i])) {
+            newArr = newArr.concat(flatten(oldArr[i]))
+        } else {
+            newArr.push(oldArr[i])
+        }
+    }
+    return newArr;
+}
+
+
+  // flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
+  // flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+  // flatten([[1],[2],[3]]) // [1,2,3]
+  // flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3
