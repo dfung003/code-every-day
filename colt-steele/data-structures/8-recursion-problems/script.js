f1cd5781553e455f74d9cbeb8e95836cf73c257a
dfung003 <dfung003@ucr.edu>
2022-08-08 15:33:39 -0700

    capitalize words solution

--- colt-steele/data-structures/8-recursion-problems/script.js
+++ colt-steele/data-structures/8-recursion-problems/script.js
@@ -101,7 +101,18 @@ function flatten(oldArr) {
 }
 
 
-  // flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
-  // flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
-  // flatten([[1],[2],[3]]) // [1,2,3]
-  // flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3
+// flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
+// flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+// flatten([[1],[2],[3]]) // [1,2,3]
+// flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3
+
+function capitalizeWords(array) {
+    if (array.length === 1) {
+        return [array[0].toUpperCase()];
+    }
+    let res = capitalizeWords(array.slice(0, -1));
+    res.push(array.slice(array.length - 1)[0].toUpperCase());
+    return res;
+
+}
+capitalizeFirst(['car', 'taco', 'banana']); // ['Car','Taco','Banana']
