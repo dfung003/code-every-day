97b22095ea824f341b1cc591d0fcde5107e45402
dfung003 <dfung003@ucr.edu>
2022-08-10 10:41:06 -0700

    intro to sorting algos

---
+++ colt-steele/data-structures/10-bubble-sort/script.js
@@ -0,0 +1,19 @@
+// Intro to Sorting Algorithms \\
+
+// What is sorting?
+
+// Sorting is the process of rearranging the items in a collection (e.g. an array) so that the items are in some kind of order.
+
+// Examples
+/*
+- Sorting numbers from smallest to largest
+- Sorting names alphabetically
+- Sorting movies based on release year
+- Sorting movies based on revenue
+*/
+
+// Why do we need to learn this?
+/*
+- Sorting is an incredibly common task, so it's good to know how it works
+- There are many different ways to sort things, and different techniques have their own advantages and disadvantages
+*/
