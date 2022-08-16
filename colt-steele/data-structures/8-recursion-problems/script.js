7c631da4fb8afabe967ec61a3bc3646bb808c6b4
dfung003 <dfung003@ucr.edu>
2022-08-16 13:55:01 -0700

    collectStrings solution

--- colt-steele/data-structures/8-recursion-problems/script.js
+++ colt-steele/data-structures/8-recursion-problems/script.js
@@ -207,3 +207,40 @@ stringifyNumbers(obj)
     }
 }
 */
+
+function collectStrings(obj) {
+    var stringsArr = [];
+
+    function gatherStrings(o) {
+        for (var key in o) {
+            if (typeof o[key] === 'string') {
+                stringsArr.push(o[key]);
+            }
+            else if (typeof o[key] === 'object') {
+                return gatherStrings(o[key]);
+            }
+        }
+    }
+
+    gatherStrings(obj);
+
+    return stringsArr;
+}
+
+const obj = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+}
+
+collectStrings(obj) // ["foo", "bar", "baz"])
