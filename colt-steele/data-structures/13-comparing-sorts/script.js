3e9ff74523b62b5b7e7f02b3366829ff8b99eefd
dfung003 <dfung003@ucr.edu>
2022-08-21 13:30:59 -0700

    comparing selection, bubble, insertion sorts

---
+++ colt-steele/data-structures/13-comparing-sorts/script.js
@@ -0,0 +1,21 @@
+// Big O of Sorting Algorithms \\
+
+// Bubble Sort
+// Time Complexity O(n) Best, O(n^2) Average, O(n^2) Worst
+// Space Complexity O(1)
+
+// Insertion Sort
+// Time Complexity O(n) Best, O(n^2) Average, O(n^2) Worst
+// Space Complexity O(1)
+
+// Selection Sort
+// Time Complexity O(n^2) Best, O(n^2) Average, O(n^2) Worst
+// Space Complexity O(1)
+
+// Recap
+/*
+- Sorting is fundamental!
+- Bubble sort, selection sort, and insertion sort are all roughly equivalent
+- All have average time complexities that are quadratic
+- We can do better... but we need more complex algorithms!
+*/
