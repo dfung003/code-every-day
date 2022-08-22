014148244f563e3f5b2bcd69bb3424c384b6a4ee
dfung003 <dfung003@ucr.edu>
2022-08-22 15:44:32 -0700

    nestedEvenSum solution fixed

--- colt-steele/data-structures/8-recursion-problems/script.js
+++ colt-steele/data-structures/8-recursion-problems/script.js
@@ -117,7 +117,7 @@ function capitalizeWords(array) {
 }
 capitalizeFirst(['car', 'taco', 'banana']); // ['Car','Taco','Banana']
 
-
+// Write a recursive function called nestedEvenSum. Return the sum of all even numbers in an object which may contain nested objects.
 function nestedEvenSum(obj, sum = 0) {
     for (var key in obj) {
         if (typeof obj[key] === 'object') {
