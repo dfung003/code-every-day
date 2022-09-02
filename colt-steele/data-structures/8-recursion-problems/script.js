616efe3d5ab5e7b1e6ba246995e5a3181dd51b18
dfung003 <dfung003@ucr.edu>
2022-09-02 16:00:11 -0700

    collectStrings solution fixed

--- colt-steele/data-structures/8-recursion-problems/script.js
+++ colt-steele/data-structures/8-recursion-problems/script.js
@@ -212,6 +212,7 @@ stringifyNumbers(obj)
 }
 */
 
+// Write a function called collectStrings which accepts an object and returns an array of all the values in the object that have a typeof string
 function collectStrings(obj) {
     var stringsArr = [];
 
