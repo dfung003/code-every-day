5c250bf24b2df30782db6a2b9ed1845ab4f78314
dfung003 <dfung003@ucr.edu>
2022-09-01 17:06:14 -0700

    stringify numbers solution fixed

--- colt-steele/data-structures/8-recursion-problems/script.js
+++ colt-steele/data-structures/8-recursion-problems/script.js
@@ -166,6 +166,8 @@ function capitalizeFirst(array) {
 }
 capitalizeFirst(['car', 'taco', 'banana']); // ['Car','Taco','Banana']
 
+
+// Write a function called stringifyNumbers which takes in an object and finds all of the values which are numbers and converts them to strings. Recursion would be a great way to solve this!
 function stringifyNumbers(obj) {
     var newObj = {};
     for (var key in obj) {
