bd8a6391b4327004718aca4acf2e70585e5e7594
dfung003 <dfung003@ucr.edu>
2022-09-08 15:41:55 -0700

    radix sort pseudocode 2

--- colt-steele/data-structures/16-radix-sort/script.js
+++ colt-steele/data-structures/16-radix-sort/script.js
@@ -83,6 +83,12 @@ mostDigits([23, 567, 89, 12234324, 90]) // 8 because largest number has 8 digits
 
 // Radix Sort Pseudocode
 /*
-- Define a function that accepts list of numbers
+- Define a function that accepts lists of numbers
 - Figure out how many digits the largest number has
+- Loop from k = 0 up to this largest number of digits
+- For each iteration of the loop:
+    - Create buckets for each digit (0 to 9)
+    - Place each number in the corresponding bucket based on its kth digit
+- Replace our existing array with values in our buckets, starting with 0 and going up to 9
+- Return list at the end!
 */
