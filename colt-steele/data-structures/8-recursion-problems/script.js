011d71926ffb3d7214454918d007296cf1893caf
dfung003 <dfung003@ucr.edu>
2022-09-17 22:01:29 -0700

    reverse function fixed

--- colt-steele/data-structures/8-recursion-problems/script.js
+++ colt-steele/data-structures/8-recursion-problems/script.js
@@ -52,6 +52,7 @@ function fib(n) {
     return fib(n - 1) + fib(n - 2);
 }
 
+// Write a recursive function called reverse which accepts a string and returns a new string in reverse.
 function reverse(str) {
     if (str.length <= 1) return str;
     return reverse(str.slice(1)) + str[0];
