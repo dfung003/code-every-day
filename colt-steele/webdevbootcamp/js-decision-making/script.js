6d9f9f0d21cfb020e14e73b6cc5ccec7361c5184
dfung003 <dfung003@ucr.edu>
2022-09-20 16:33:49 -0700

    js boolean logic greater, less, equal

---
+++ colt-steele/webdevbootcamp/js-decision-making/script.js
@@ -0,0 +1,31 @@
+// JavaScript Decision Making \\
+
+// Boolean Logic, Making decisions with JavaScript
+// Comparisons
+/*
+> greater than
+< less than
+>= greater than or equal to
+<= less than or equal to
+== equality
+!= not equal
+=== strict equality
+!== strict non-equality
+*/
+
+// Examples
+/*
+10 > 1 true
+0.2 > 0.3 false
+-10 < 0 true
+50.5 < 5 false
+0.5 <= 0.5 true
+99 >= 4 true
+99 >= 99 true
+'a' < 'b' true
+'A' > 'a' false
+
+Notice these all return a Boolean!
+
+Though it's uncommon, you can compare strings. Just be careful, things get dicey when dealing with case, special characters, and accents!
+*/
