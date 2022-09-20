51060713965112526b4e93e6b7330c4801fe6b54
dfung003 <dfung003@ucr.edu>
2022-09-19 17:04:02 -0700

    data structures intro

---
+++ colt-steele/data-structures/17-data-struct-intro/script.js
@@ -0,0 +1,24 @@
+// Data Structures Introduction \\
+
+// What do they do?
+// Data structures are collections of values, the relationships among them, and the functions or operations that can be applied to the data
+
+// Why so many??
+// Different data structures excel at different things. Some are highly specialized, while others (like arrays) are more generally used
+
+// Why care??
+// The more time you spend as a developer, the more likely you'll need to use one of these data structures
+// You've already worked with many of them unknowingly!
+// And of course... Interviews!
+
+// They all excell in different situations...
+// Otherwise why bother learning them all??
+
+// Working with map/location data?
+// Use a graph!
+
+// Need an ordered list with fast inserts/removals at the beginning and end?
+// Use a linked list!
+
+// Need to write a scheduler?
+// Use a binary heap!
