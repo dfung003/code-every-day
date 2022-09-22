c3360f2e8876315965c173095bb18f52c7e1a6a8
dfung003 <dfung003@ucr.edu>
2022-09-22 15:37:05 -0700

    boolean quiz

--- colt-steele/webdevbootcamp/js-decision-making/script.js
+++ colt-steele/webdevbootcamp/js-decision-making/script.js
@@ -58,3 +58,25 @@ false === 0; // false
 Same applies for != and !==
 10 != '10' // false
 10 !== '10'; // true
+*/
+
+/*
+Question 1:
+What does the following code evaluate to?
+
+3 <= 2
+false
+
+Question 2:
+What does the following evaluate to?
+
+9 == '9'
+true
+
+Question 3:
+What does the following code evaluate to?
+
+-5 >= -6
+true
+*/
+
