9de835621ddc5a786d4b4385dfb9f5335aeef313
dfung003 <dfung003@ucr.edu>
2022-10-03 15:00:46 -0700

    else if statements

--- colt-steele/webdevbootcamp/js-decision-making/script.js
+++ colt-steele/webdevbootcamp/js-decision-making/script.js
@@ -146,3 +146,15 @@ function isEven(num) {
     }
 
 }
+
+// Else If
+// If not the first thing, maybe this other thing??
+
+let rating = 2;
+
+if (rating === 3) {
+    console.log("YOU ARE A SUPERSTAR!");
+}
+else if (rating === 2) {
+    console.log("MEETS EXPECTATIONS");
+}
