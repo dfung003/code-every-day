9b2fd9c4ebf51e16ed033405ced7803d9c47a7e0
dfung003 <dfung003@ucr.edu>
2022-10-13 18:18:47 -0700

    nesting conditionals

--- colt-steele/webdevbootcamp/js-decision-making/script.js
+++ colt-steele/webdevbootcamp/js-decision-making/script.js
@@ -215,3 +215,19 @@ function getColor(phrase) {
         console.log("purple")
     }
 }
+
+// Nesting
+// We can nest conditionals inside conditionals
+
+let password = "cat dog";
+if (password.length >= 6) {
+    if (password.indexOf(' ') !== -1) {
+        console.log("Password cannot include spaces");
+    }
+    else {
+        console.log("Valid password!!")
+    }
+}
+else {
+    console.log("Password too short!");
+}
