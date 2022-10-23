5f0dfb4a9649f51c6b8e7d695c60e838ceec9953
dfung003 <dfung003@ucr.edu>
2022-10-23 16:22:04 -0700

    AND statements

--- colt-steele/webdevbootcamp/js-decision-making/script.js
+++ colt-steele/webdevbootcamp/js-decision-making/script.js
@@ -301,3 +301,12 @@ if (' ') {
 } else {
     console.log("FALSY!")
 }
+
+// AND
+// Both sides must be true, for the entire thing to be true
+
+// 1 <= 4 && 'a' === 'a'; true
+
+// 9 > 10 && 9 >= 9; false
+
+// 'abc'.length === 3 && 1+1 === 4; false
