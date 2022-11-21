fd316babca7e411f2bb63f56dd6c6cbb4871fbe2
dfung003 <dfung003@ucr.edu>
2022-11-20 19:13:43 -0800

    creating arrays notes

--- colt-steele/webdevbootcamp/js-arrays/script.js
+++ colt-steele/webdevbootcamp/js-arrays/script.js
@@ -7,3 +7,17 @@
 - Collections of levels in a game
 - Songs in a playlist
 */
+
+// Creating Arrays
+
+// To make an empty array
+let students = [];
+
+// An array of strings
+let colors = ['red', 'orange', 'yellow'];
+
+// An array of numbers
+let lottoNums = [19, 22, 56, 12, 51];
+
+// A mixed array
+let stuff = [true, 68, 'cat', null];
