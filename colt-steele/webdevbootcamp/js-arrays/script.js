72689c7b63683c424c77813a61c0c4371d6c78fa
dfung003 <dfung003@ucr.edu>
2022-12-22 16:34:01 -0800

    slice colors example

--- colt-steele/webdevbootcamp/js-arrays/script.js
+++ colt-steele/webdevbootcamp/js-arrays/script.js
@@ -117,3 +117,8 @@ dogs.reverse();
 
 // Slice method
 // The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified.
+
+let colors3 = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+colors3.slice(3);
+// ['green', 'blue', 'indigo', 'violet']
