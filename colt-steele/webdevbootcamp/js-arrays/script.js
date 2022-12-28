108abc802f741aba366d9cd4f831351f14aeb5b2
dfung003 <dfung003@ucr.edu>
2022-12-28 11:03:43 -0800

    months example for splice

--- colt-steele/webdevbootcamp/js-arrays/script.js
+++ colt-steele/webdevbootcamp/js-arrays/script.js
@@ -130,3 +130,9 @@ let warmColors = colors3.slice(0, 3)
 
 // Splice Method
 // The splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place
+
+const months = ['Jan', 'March', 'April', 'June'];
+months.splice(1, 0, 'Feb');
+// inserts at index 1
+console.log(months);
+// expected output: Array ['Jan', 'Feb', 'March', 'April', 'June']
