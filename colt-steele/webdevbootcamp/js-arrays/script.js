7474495f5281ae76a0a580c97dd8b5146710f252
dfung003 <dfung003@ucr.edu>
2023-01-26 16:24:52 -0800

    3

--- colt-steele/webdevbootcamp/js-arrays/script.js
+++ colt-steele/webdevbootcamp/js-arrays/script.js
@@ -158,7 +158,7 @@ let refCopy = [1, 2, 3]
 // refNums === refCopy
 // true
 
-// Arrays and Const
+// Arrays & Const
 // The values can change as long as the reference remains the same
 // const cares about the variable not being reassigned
 
